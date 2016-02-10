940adfc164bd1c3b48f2355ed5742bcb9ca17cc3
Ander Ipartek <auraga@ipartek.com>
2016-02-10 10:16:01 +0100

    Initial Import

---
+++ src/main/java/com/ipartek/formacion/backoffice/backoffice/App.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.backoffice.backoffice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
